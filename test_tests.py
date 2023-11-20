7ab0fb79c0b481d97fbc0f0f7c048969f3d2ec71
Alexandra Zvolinskaya <risa.infinita@gmail.com>
2023-11-20 16:50:30 +0300

    autotests for class BooksCollector written in file test_tests.py

--- test_tests.py
+++ test_tests.py
@@ -3,19 +3,18 @@ from main import BooksCollector
 
 
 class TestBooksCollector:
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_add_one_book(self):
 
         collector = BooksCollector()
-        collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-        assert len(collector.books_genre.keys()) == 2
+        assert 'Что делать, если ваш кот хочет вас убить' in collector.get_books_genre()
 
     def test_add_new_book_add_same_book_twice_not_added(self):
 
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Гордость и предубеждение и зомби')
-        assert len(collector.books_genre.keys()) == 1
+        assert len(collector.get_books_genre()) == 1
 
     @pytest.mark.parametrize(
         'book',
@@ -29,11 +28,12 @@ class TestBooksCollector:
     def test_add_new_book_name_length_below41_success(self, book):
         collector = BooksCollector()
         collector.add_new_book(book)
-        assert len(collector.books_genre.keys()) == 1
+        assert len(collector.get_books_genre()) == 1
 
     @pytest.mark.parametrize(
         'book',
         [
+            '',
             'Длинное название в нем ровно 40+1 символ ',
             'Длинное название в нем ровно 40+2 символа',
             'Чудовищно длинное название в нем ровно 50 символов'
@@ -42,18 +42,18 @@ class TestBooksCollector:
     def test_add_new_book_name_length_over40_fail(self, book):
         collector = BooksCollector()
         collector.add_new_book(book)
-        assert len(collector.books_genre.keys()) == 0
+        assert len(collector.get_books_genre()) == 0
 
     def test_add_new_book_book_adds_without_genre_empty_genre(self):
         collector = BooksCollector()
         collector.add_new_book('Ловцы книг')
-        assert collector.books_genre['Ловцы книг'] == ''
+        assert collector.get_book_genre('Ловцы книг') == ''
 
     def test_set_book_genre_genre_of_new_book_success(self):
         collector = BooksCollector()
         collector.books_genre = {'Мистер Вуду и дни недели': ''}
         collector.set_book_genre('Мистер Вуду и дни недели', 'Фантастика')
-        assert collector.books_genre['Мистер Вуду и дни недели'] == 'Фантастика'
+        assert collector.get_book_genre('Мистер Вуду и дни недели') == 'Фантастика'
 
     def test_get_book_genre_book_has_genre_success(self):
         collector = BooksCollector()
@@ -104,19 +104,19 @@ class TestBooksCollector:
         collector = BooksCollector()
         collector.books_genre = {'Сказки старого Вильнюса': 'Фантастика'}
         collector.add_book_in_favorites('Сказки старого Вильнюса')
-        assert 'Сказки старого Вильнюса' in collector.favorites
+        assert 'Сказки старого Вильнюса' in collector.get_list_of_favorites_books()
 
     def test_add_book_in_favorites_same_book_added_fail(self):
         collector = BooksCollector()
         collector.favorites = ['Сказки старого Вильнюса']
         collector.add_book_in_favorites('Сказки старого Вильнюса')
-        assert len(collector.favorites) == 1
+        assert len(collector.get_list_of_favorites_books()) == 1
 
     def test_delete_book_from_favorites_one_book_deleted_success(self):
         collector = BooksCollector()
         collector.favorites = ['Сказки старого Вильнюса']
         collector.delete_book_from_favorites('Сказки старого Вильнюса')
-        assert 'Сказки старого Вильнюса' not in collector.favorites
+        assert 'Сказки старого Вильнюса' not in collector.get_list_of_favorites_books()
 
     def test_get_list_of_favorites_books_current_list_success(self):
         collector = BooksCollector()
